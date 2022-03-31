38b8009e6fc781927e1e6239ca0cb6ea7f9362a7
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-31 21:50:55 +0300

    cors

--- automatedDonation/settings.py
+++ automatedDonation/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'donationApp',
     'rest_framework',
     'rest_framework.authtoken'
-    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -57,7 +56,6 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,41 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-]
-
-CORS_ALLOWED_ORIGINS = [
-"https://domain.com",
-"https://api.domain.com",
-"http://localhost:8080",
-"http://127.0.0.1:9000",
-"http://localhost:4200",
-]
-# CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-r"^https://\w+\.domain\.com$",
-]
-
-CORS_ALLOW_METHODS = [
-'DELETE',
-'GET',
-'OPTIONS',
-'PATCH',
-'POST',
-'PUT',
-]
-
-CORS_ALLOW_HEADERS = [
-'accept',
-'accept-encoding',
-'authorization',
-'content-type',
-'dnt',
-'origin',
-'user-agent',
-'x-csrftoken',
-'x-requested-with',
 ]
 
 ROOT_URLCONF = 'automatedDonation.urls'
