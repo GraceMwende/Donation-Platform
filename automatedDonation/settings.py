48758ed67c1198839fe74db7c1a1cbca2daf103c
GraceMwende <grace.micheni@student.moringaschool.com>
2022-04-01 13:37:47 +0300

    cors

--- automatedDonation/settings.py
+++ automatedDonation/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
